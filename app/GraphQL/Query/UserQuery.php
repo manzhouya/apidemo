85fbb156abd1656e97f98f70e53f340ec3ba8d19
manzhou <manzhouya@gmail.com>
2018-11-05 18:05:11 +0800

    graphql basic use

---
+++ app/GraphQL/Query/UserQuery.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use App\User;
+use Folklore\GraphQL\Support\Query;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use GraphQL;
+
+class UserQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'users',
+    ];
+
+    public function type()
+    {
+        return Type::listOf(GraphQL::type('User'));
+    }
+
+    public function args()
+    {
+        return [
+            'id' => ['name' => 'id', 'type' => Type::string()],
+            'email' => ['name' => 'email', 'type' => Type::string()],
+            'created_at' => ['name' => 'created_at', 'type' => Type::string()],
+        ];
+    }
+
+    public function resolve($root, $args, $context, ResolveInfo $info)
+    {
+        if (isset($args['id'])) {
+            return User::where('id', $args['id'])->get();
+        } elseif (isset($args['email'])) {
+            return User::where('email', $args['email'])->get();
+        } else {
+            return User::all();
+        }
+    }
+}
---
+++ app/GraphQL/Type/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as BaseType;
+use GraphQL;
+
+class UserType extends BaseType
+{
+    protected $attributes = [
+        'name' => 'User',
+        'description' => 'A User'
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The id of the user'
+            ],
+            'email' => [
+                'type' => Type::string(),
+                'description' => 'The email of the user'
+            ],
+            'created_at' => [
+                'type' => Type::string(),
+                'description' => 'The email of the user'
+            ],
+        ];
+    }
+
+    protected function resolveEmailField($root, $args)
+    {
+        return strtolower($root->email);
+    }
+
+    protected function resolveCreatedAtField($root, $args)
+    {
+        return strtolower($root->created_at);
+    }
+}
